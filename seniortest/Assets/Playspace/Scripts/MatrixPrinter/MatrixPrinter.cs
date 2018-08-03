988070e103659e0afc14a5fac7ca8049b1e46216
Ramon Ferreira <ramon_felipe@outlook.com>
2018-08-03 01:05:54 -0300

    submiting second exercise

---
+++ seniortest/Assets/Playspace/Scripts/MatrixPrinter/MatrixPrinter.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MatrixPrinter
+{
+    public static class MatrixPrinter
+    {
+        // Function print matrix in spiral form
+        public static string MatrixClockwised(int endingRowIndex, int endingColumnIndex, int[,] matrix)
+        {
+            int i;
+            int rowIndex = 0, columnIndex = 0;
+            string result = "";
+
+            while (rowIndex < endingRowIndex && columnIndex < endingColumnIndex)
+            {
+                for (i = columnIndex; i < endingColumnIndex; ++i)
+                {
+                    int value = matrix[rowIndex, i];
+
+                    result += value.ToString() + " ";
+                }
+                rowIndex++;
+
+                for (i = rowIndex; i < endingRowIndex; ++i)
+                {
+                    int value = matrix[i, endingColumnIndex - 1];
+
+                    result += value.ToString() + " ";
+                }
+                endingColumnIndex--;
+
+                if (rowIndex < endingRowIndex)
+                {
+                    for (i = endingColumnIndex - 1; i >= columnIndex; --i)
+                    {
+                        int value = matrix[endingRowIndex - 1, i];
+                        result += value.ToString() + " ";
+                    }
+                    endingRowIndex--;
+                }
+
+                if (columnIndex < endingColumnIndex)
+                {
+                    for (i = endingRowIndex - 1; i >= rowIndex; --i)
+                    {
+                        int value = matrix[i, columnIndex];
+                        result += value.ToString() + " ";
+                    }
+                    columnIndex++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ seniortest/Assets/Playspace/Scripts/MatrixPrinter/MatrixPrinterManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MatrixPrinter
+{
+    public class MatrixPrinterManager : MonoBehaviour
+    {
+
+        [Header("Interface Components")]
+        [SerializeField] private Button _matrixPrintButton;
+        [SerializeField] private Text _matrixText;
+        [SerializeField] private Text _matrixConfigurationText;
+
+        [Header("Matrix")]
+        [HideInInspector] private int[,] _matrix = 
+                                                    { 
+                                                        {1, 2, 3, 4, 5},
+                                                        {6, 7, 8, 9, 10},
+                                                        {11, 12, 13, 14, 15},
+                                                        {16, 17, 18, 19, 20},
+                                                        {21, 22, 23, 24, 25}
+                                                    };
+
+        private void Awake()
+        {
+            _matrixPrintButton.onClick.AddListener(() => OnMatrixPrintButtonClicked());
+        }
+
+        private void OnMatrixPrintButtonClicked()
+        {
+            //getting matrix lengths
+            int rowsAmount = _matrix.GetLength(0);
+            int columnsAmount = _matrix.GetLength(1);
+
+            _matrixConfigurationText.text = "Rows: " + rowsAmount + "\n" + "Columns: " + columnsAmount;
+            _matrixText.text = MatrixPrinter.MatrixClockwised(rowsAmount, columnsAmount, _matrix);
+        }
+    }
+}
