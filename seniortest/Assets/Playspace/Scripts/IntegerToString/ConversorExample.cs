fad53c18eab8d8680c8989e73735fa19e94c2eab
Ramon Ferreira <ramon_felipe@outlook.com>
2018-08-03 00:11:42 -0300

    First exercise done

---
+++ seniortest/Assets/Playspace/Scripts/IntegerToString/ConversorExample.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace IntegerToString
+{
+    public class ConversorExample : MonoBehaviour
+    {
+        [Header("Interface Components")]
+        [SerializeField] private InputField _integerInputField;
+        [SerializeField] private InputField _baseWantedInputField;
+        [SerializeField] private Button _convertButton;
+        [SerializeField] private Text _stringResultText;
+
+        private void Awake()
+        {
+            _convertButton.onClick.AddListener(() => OnConvertButtonClicked(_integerInputField.text, _baseWantedInputField.text));
+        }
+
+        private void OnConvertButtonClicked(string inputValue, string baseWanted)
+        {
+            try
+            {
+                int intToConvert;
+                int baseToUse;
+
+                if (int.TryParse(inputValue, out intToConvert) && int.TryParse(baseWanted, out baseToUse))
+                {
+                    _stringResultText.text = CustomConversor.Itoa(intToConvert, baseToUse);
+                }
+                else
+                {
+                    _stringResultText.text = "A problem occur setting your conversion. Try another number.";
+                }
+            }
+            catch (System.Exception e)
+            {
+                _stringResultText.text = e.Message;
+            }
+        }
+    }
+}
---
+++ seniortest/Assets/Playspace/Scripts/IntegerToString/CustomConversor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+
+namespace IntegerToString
+{
+    public static class CustomConversor
+    {
+        public static String Itoa(int number, int wantedBase)
+        {
+            //cheking for zero
+            if (number == 0)
+                return "0";
+
+            //needed values
+            int index = 10;
+            int range = 1 + index;
+            char[] buff = new char[range];
+            string alphabet = "0123456789abcdefghijklmnopqrstuvwxyz";
+
+            for (int i = Math.Abs(number), q; i > 0; i = q)
+            {
+                q = Math.DivRem(i, wantedBase, out i);
+                buff[index -= 1] = alphabet[i];
+            }
+
+            if (number < 0)
+            {
+                buff[index -= 1] = '-';
+            }
+
+            return new String(buff, index, buff.Length - index);
+        }
+
+        public static void TestMethod()
+        {
+            Debug.Log(Itoa(-0x12345678, 16));
+        }
+    }
+}
