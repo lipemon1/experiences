d98a53329072b60e05fa9b20f87a071156596342
Ramon Ferreira <ramon_felipe@outlook.com>
2018-08-03 01:42:35 -0300

    removing imports not used

--- seniortest/Assets/Playspace/Scripts/MatrixPrinter/MatrixPrinter.cs
+++ seniortest/Assets/Playspace/Scripts/MatrixPrinter/MatrixPrinter.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-namespace MatrixPrinter
+﻿namespace MatrixPrinter
 {
     public static class MatrixPrinter
     {
